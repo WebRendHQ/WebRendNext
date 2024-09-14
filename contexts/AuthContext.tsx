1475dba45f67cba1d9178a41f7258275c803dc2f
Kevin Vera <ceo@webrend.com>
2024-09-14 16:25:21 -0400

    fixed errors for deployment on github pages

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -2,9 +2,16 @@ import React, { createContext, useContext, useState, useEffect } from 'react'
 import { initializeApp } from 'firebase/app'
 import { getAuth, onAuthStateChanged, User } from 'firebase/auth'
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  // Your Firebase configuration here
-}
+  apiKey: "AIzaSyCJM45UvPM9DPxnUy7hCfoMN2Ft4wV9o40",
+  authDomain: "webrend-f3469.firebaseapp.com",
+  projectId: "webrend-f3469",
+  storageBucket: "webrend-f3469.appspot.com",
+  messagingSenderId: "628574920834",
+  appId: "1:628574920834:web:c3bd854a61a9b5caa7949b",
+  measurementId: "G-QHNJHR0KCP"
+};
 
 const app = initializeApp(firebaseConfig)
 const auth = getAuth(app)
